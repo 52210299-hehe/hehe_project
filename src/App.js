8128ac6a4d9e8964743b5d14431ed3ed21ce0855
ahmad ayoubi <52210299@students.liu.edu.lb>
2025-11-23 14:47:59 +0200

    created routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
-
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar.jsx'
+import About from './components/About.jsx'
+import Home from './components/Home.jsx'
+import Manage from './components/Manage.jsx'
+import Register from './components/register.jsx'
+import Login from './components/login.jsx'
+import Footer from './components/Footer.jsx'
 function App() {
   
   return (
     <div className=''>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/manage_travels' element={<Manage />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+      <Footer />
       
 
     </div>
